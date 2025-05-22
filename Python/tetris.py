95d173e1066f7b56d6d82791b2f69bc288c5a469
Leander Jakschik <leander.jakschik@student.tu-freiberg.de>
2025-05-22 17:24:03 +0200

    hab mal paar fehler behoben, nh
    Wäre halt doof wenn die nur Windows spezifisch waren, aber ich glaube nicht

--- Python/tetris.py
+++ Python/tetris.py
@@ -5,6 +5,7 @@ from ulib import remote
 from dataclasses import dataclass
 import time
 
+
 @dataclass
 class Vec:
     x: int
@@ -16,6 +17,7 @@ class Vec:
     def __sub__(self, other):
         return Vec(self.x - other.x, self.y - other.y)
 
+
 inputs = {
     "left": False,
     "right": False,
@@ -23,7 +25,7 @@ inputs = {
     "up": False,
     "space": False,
     "enter": False,
-    "esc": False,
+    "escape": False,
 }
 
 current_shape_matrix = np.zeros((3, 3))
@@ -35,35 +37,25 @@ running = False
 score = 0
 
 shapes = [
-np.array([
-    [1, 0, 0],
-    [1, 1, 1],
-    [0, 0, 0],
-]).T, np.array([
-    [0, 1, 1],
-    [1, 1, 0],
-    [0, 0, 0]
-]).T, np.array([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [1, 1, 1, 1],
-    [0, 0, 0, 0]
-]).T, np.array([
-    [0, 1, 0],
-    [1, 1, 1],
-    [0, 0, 0]
-]).T, np.array([
-    [1, 1],
-    [1, 1],
-]).T, np.array([
-    [1, 1, 0],
-    [0, 1, 1],
-    [0, 0, 0]
-]).T, np.array([
-    [0, 0, 1],
-    [1, 1, 1],
-    [0, 0, 0]
-]).T]
+    np.array(
+        [
+            [1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0],
+        ]
+    ).T,
+    np.array([[0, 1, 1], [1, 1, 0], [0, 0, 0]]).T,
+    np.array([[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]]).T,
+    np.array([[0, 1, 0], [1, 1, 1], [0, 0, 0]]).T,
+    np.array(
+        [
+            [1, 1],
+            [1, 1],
+        ]
+    ).T,
+    np.array([[1, 1, 0], [0, 1, 1], [0, 0, 0]]).T,
+    np.array([[0, 0, 1], [1, 1, 1], [0, 0, 0]]).T,
+]
 
 colors = [
     (255, 0, 0),
@@ -71,31 +63,35 @@ colors = [
     (0, 0, 255),
     (100, 100, 0),
     (0, 100, 100),
-    (100, 0, 100)
+    (100, 0, 100),
 ]
 
 background_color = (0, 0, 0)
 pixels = np.full((16, 16, 3), background_color)
-    
+
+
 def setpixel(x: int, y: int, color: tuple):
     if x >= 0 and y >= 0 and x < 16 and y < 16:
         display.set_xy(x, y, color)
         pixels[x, y] = color
-    
+
+
 def getpixel(x: int, y: int):
     if x >= 0 and x < 16 and y < 16:
-        if (y >= 0):
+        if y >= 0:
             return tuple(pixels[x, y])
         else:
             return background_color
     else:
         return (255, 255, 255)
-    
+
+
 def fill(color: tuple):
     display.fill(color)
     for y in range(16):
         for x in range(16):
             pixels[x, y] = color
+
 
 def set_shape(shape_matrix: np.ndarray, offset: Vec, color: tuple):
     for x in range(shape_matrix.shape[0]):
@@ -103,24 +99,31 @@ def set_shape(shape_matrix: np.ndarray, offset: Vec, color: tuple):
             if shape_matrix[x, y] == 0:
                 continue
             setpixel(offset.x + x, offset.y + y, color)
-            
+
+
 def get_rotated_shape_matrix(shape_matrix: np.ndarray, isleft: bool):
     if isleft:
         return np.rot90(shape_matrix, 1)
     else:
         return np.rot90(shape_matrix, -1)
-    
+
+
 def check_fit(shape_matrix: np.ndarray, position: Vec):
     for x in range(shape_matrix.shape[0]):
         for y in range(shape_matrix.shape[1]):
-            if shape_matrix[x, y] == 1 and getpixel(position.x + x, position.y + y) != background_color:
+            if (
+                shape_matrix[x, y] == 1
+                and getpixel(position.x + x, position.y + y) != background_color
+            ):
                 return False
     return True
-            
+
+
 def remove_row(row: int):
     for y in range(row, 0, -1):
         for x in range(16):
             setpixel(x, y, getpixel(x, y - 1))
+
 
 def check_for_full_row():
     global score
@@ -138,12 +141,15 @@ def check_for_full_row():
         block_counter = 0
     score += local_score * score_multiplier
 
+
 def paste_current_shape():
     set_shape(current_shape_matrix, current_shape_position, current_shape_color)
 
+
 def cut_current_shape():
     set_shape(current_shape_matrix, current_shape_position, background_color)
-                
+
+
 def move_horizontal(isleft: bool):
     global current_shape_position
     moved_position = current_shape_position
@@ -156,14 +162,15 @@ def move_horizontal(isleft: bool):
         current_shape_position = moved_position
     paste_current_shape()
 
+
 def move_down():
     global current_shape_position
     moved_position = current_shape_position + Vec(0, 1)
     cut_current_shape()
     if not check_fit(current_shape_matrix, moved_position):
-        if (current_shape_position.y < 0):
+        if current_shape_position.y < 0:
             print("game over")
-            inputs["esc"] = True
+            inputs["escape"] = True
         else:
             paste_current_shape()
             check_for_full_row()
@@ -171,7 +178,8 @@ def move_down():
         return
     current_shape_position = moved_position
     paste_current_shape()
-    
+
+
 def rotate(isleft: bool):
     global current_shape_matrix
     rotated_matrix = get_rotated_shape_matrix(current_shape_matrix, isleft)
@@ -179,7 +187,8 @@ def rotate(isleft: bool):
     if check_fit(rotated_matrix, current_shape_position):
         current_shape_matrix = rotated_matrix
     paste_current_shape()
-    
+
+
 def get_new_shape():
     global fall_step_interval_seconds, current_shape_matrix, current_shape_position, current_shape_color
     current_shape_position = Vec(6, -2)
@@ -188,15 +197,19 @@ def get_new_shape():
     paste_current_shape()
     fall_step_interval_seconds *= 0.98
 
+
 def register_input(key: str):
     global running
     if key in inputs:
         inputs[key] = True
-    if key == "exit": running = False   #exit-flag, wird von pygame gesetzt, könnte man aus dem code werfen später
+    if key == "exit":
+        running = False  # exit-flag, wird von pygame gesetzt, könnte man aus dem code werfen später
+
 
 def reset_inputs():
     for key in inputs:
         inputs[key] = False
+
 
 def start_tetris():
     print("entered tetris game")
@@ -213,25 +226,26 @@ def start_tetris():
             rotate(inputs["up"])
         if inputs["down"] or inputs["enter"]:
             move_down()
-        if inputs["esc"]:
+        if inputs["escape"]:
             break
         reset_inputs()
         time.sleep(0.1)
         display.show()
     reset_inputs()
 
+
 def start_main_menu():
     global running
     print("entered main menu")
-    fill((20, 20, 20))
+    fill((20, 120, 20))
     while running:
-        if inputs["esc"]:
+        if inputs["escape"]:
             running = False
         if inputs["space"]:
             return True
         reset_inputs()
-        time.sleep(0.1) #muss hier sein, damit das programm ich schließen lässt
-        #start menu code here...
+        time.sleep(0.1)  # muss hier sein, damit das programm ich schließen lässt
+        # start menu code here...
     return False
 
 
@@ -245,9 +259,6 @@ def main():
     running = False
     remote.unbind_all(register_input)
     fill(background_color)
-    
-
-
 
 
 # program
--- Python/ulib/pygame_emulator.py
+++ Python/ulib/pygame_emulator.py
@@ -21,6 +21,7 @@ all_binds = []
 # Lock für Thread-Sicherheit
 lock = threading.Lock()
 
+
 def pygame_emu():
     global running, window, flip
     pygame.init()
@@ -42,22 +43,28 @@ def pygame_emu():
             pygame.display.flip()
             flip = False
         time.sleep(0.01)
-    pygame.quit()
+    print("closing")
+    pygame.display.quit()
+    print("quit window")
+
 
 # Starte Pygame Thread einmal
 def start_pygame_thread():
     thread = threading.Thread(target=pygame_emu, daemon=True)
     thread.start()
 
+
 def set_xy(x: int, y: int, color: tuple):
     if 0 <= x < WIDTH and 0 <= y < HEIGHT:
         with lock:
             pixels_cache[x, y] = color
 
+
 def fill(color: tuple):
     with lock:
         pixels_cache[:, :] = color
     show()
+
 
 def show():
     global window, flip
@@ -65,13 +72,18 @@ def show():
     with lock:
         for y in range(HEIGHT):
             for x in range(WIDTH):
-                pygame.draw.rect(window, tuple(pixels_cache[x, y]), pygame.Rect(x * SCALE, y * SCALE, SCALE, SCALE))
+                pygame.draw.rect(
+                    window,
+                    tuple(pixels_cache[x, y]),
+                    pygame.Rect(x * SCALE, y * SCALE, SCALE, SCALE),
+                )
     flip = True
-    
+
 
 def close():
     global running
     running = False
+
 
 def trigger_action(key):
     for action in all_binds:
@@ -80,14 +92,17 @@ def trigger_action(key):
         for action in bindings[key]:
             action(key)
 
+
 def listen():
     global listening
     listening = True
+
 
 def bind_key(key, action):
     if key not in bindings:
         bindings[key] = []
     bindings[key].append(action)
+
 
 def unbind_key(key, action):
     if key in bindings and action in bindings[key]:
@@ -95,8 +110,10 @@ def unbind_key(key, action):
         if not bindings[key]:
             del bindings[key]
 
+
 def bind_all(action):
     all_binds.append(action)
+
 
 def unbind_all(action):
     if action in all_binds:
