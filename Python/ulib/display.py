422cc92278aa3d2cde9bececc82a9fb6be7b5df9
Leander Jakschik <leander.jakschik@student.tu-freiberg.de>
2025-06-05 19:08:07 +0200

    fix api interface

--- Python/ulib/display.py
+++ Python/ulib/display.py
@@ -1,10 +1,13 @@
 from ulib import pygame_emulator
 
-def set_xy(x: int, y: int, color: tuple):
-    pygame_emulator.set_xy(x, y, color)
+
+def set_xy(pixel: tuple, color: tuple):
+    pygame_emulator.set_xy(*pixel, color)
+
 
 def fill(color: tuple):
     pygame_emulator.fill(color)
+
 
 def show():
     pygame_emulator.show()
