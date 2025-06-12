5e78f8aebdf455ec4d4abe03d6c2ef5d4a3e766f
Leander Jakschik <leander.jakschik@student.tu-freiberg.de>
2025-06-12 21:34:17 +0200

    welp too bad

--- Python/pong.py
+++ Python/pong.py
@@ -2,8 +2,12 @@ import numpy as np
 from ulib import graphics_library as gl
 from ulib import input_library as il
 
+running = False
+
 
 def start_pong():
+    global running
+    running = True
     pass
 
 
