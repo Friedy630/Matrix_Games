f942fb50866a2d33781066951c3e49a6d197f859
Friedemann KLinke <friedy630@gmail.com>
2025-06-26 18:17:48 +0100

    classified dino

--- Python/dino.py
+++ Python/dino.py
@@ -1,176 +1,182 @@
 import numpy as np
 import random as rng
-from ulib import display, remote
+from ulib import remote, game_library as game
 import time
 from ulib import graphics_library as gl
 from ulib import input_library as il
 
-is_game_over = False
+class Dino(game.Game):
+    def __init__(self):
+        super().__init__()
+        self.max_jmp_height = 5
+        self.jump_height = 0
+        self.base_height = 4
 
-def restart():
-    for obst in obstacle_instances:
-        gl.set_shape(obstacles[obst[1]], obst[0], gl.colors["background"])
-    obstacle_instances.clear()
+        self.player_pos = gl.Vec(2, 5)
 
-max_jmp_height = 8
-jump_height = 0
-base_height = 4
+        self.obs_spawn_interval_min = 0.6     # in seconds
+        self.obs_spawn_interval_max = 1.3     # in seconds
 
-player_pos = gl.Vec(2, 5)
+        self.ground_color = gl.colors["green"]
+        self.player_color = gl.colors["red"]
+        self.obstacle_color = gl.colors["white"]
 
-obs_spawn_interval_min = 0.8     # in seconds
-obs_spawn_interval_max = 1.5     # in seconds
+        # clock 1: player redraw clock
+        self.last_player_step_time = 0.0
+        self.player_step_interval = 0.01
 
-ground_color = gl.colors["green"]
-player_color = gl.colors["red"]
-obstacle_color = gl.colors["white"]
+        # clock 2: jump clock, so a jump cannot start when one is already in progress
+        self.last_jump_time = 0.0
+        self.jump_duration = 0.7
 
-# clock 1: player redraw clock
-last_player_step_time = 0.0
-player_step_interval = 0.01
+        # gets set to true, if jump is pressed right before the jump ends; "forgiving controls"
+        self.jump_directly = False
 
-def player_clock_step(current_time):
-    global last_player_step_time
-    if current_time - last_player_step_time >= player_step_interval:
-        last_player_step_time = current_time
-        player_step(current_time)
+        # clock 3: world redraw clock
+        self.last_world_step_time = 0.0
+        self.world_step_interval = 0.05
 
-# clock 2: jump clock, so a jump cannot start when one is already in progress
-last_jump_time = 0.0
-jump_duration = 0.7
+        # clock 4: obstacle spawn clock
+        self.last_spawn_time = 0.0
+        self.spawn_interval = 1
 
-# gets set to true, if jump is pressed right before the jump ends; "forgiving controls"
-jump_directly = False
+        self.player_shape = np.array(
+        [
+            [1, 1],
+            [1, 1],
+        ])
 
-def on_jump(current_time):
-    global last_jump_time, jump_directly
-    delta_time = current_time - last_jump_time
-    if delta_time >= jump_duration:
-        last_jump_time = current_time
-        jump_directly = False
-    elif jump_duration - delta_time <= 0.1:
-        jump_directly = True
+        self.spt = 0.01
 
-# clock 3: world redraw clock
-last_world_step_time = 0.0
-world_step_interval = 0.1
+        self.obstacles = [
+            np.array([
+                [0, 0],
+                [0, 0],
+                [1, 1],
+                [1, 1],
+                [1, 1]
+            ]).T,
+            np.array([
+                [0, 0],
+                [0, 0],
+                [0, 0],
+                [1, 1],
+                [1, 1]
+            ]).T,
+            np.array([
+                [0, 0],
+                [0, 0],
+                [0, 1],
+                [1, 1],
+                [1, 1]
+            ]).T,
+            np.array([
+                [0, 0],
+                [0, 0],
+                [0, 1],
+                [1, 0],
+                [0, 1]
+            ]).T,
+            np.array([
+                [0, 1],
+                [1, 1],
+                [0, 1],
+                [0, 0],
+                [0, 0],
+            ]).T
+        ]
 
-def world_clock_step(current_time):
-    global last_world_step_time
-    if current_time - last_world_step_time >= world_step_interval:
-        last_world_step_time = current_time
-        world_step()
+        self.obstacle_instances : list[tuple[gl.Vec, int]] = []
 
-# clock 4: obstacle spawn clock
-last_spawn_time = 0.0
-spawn_interval = 1
-
-def spawn_clock_step(current_time):
-    global last_spawn_time
-    if current_time - last_spawn_time >= spawn_interval:
-        last_spawn_time = current_time
-        spawn_step()
-
-def player_step(current_time):
-    global jump_height, is_game_over
-    if not check_fit(player_shape, player_pos):
-        is_game_over = True
-    gl.set_shape(player_shape, player_pos, gl.colors["background"])
-    jump_height = max(0, max_jmp_height * (-(4 / (jump_duration**2)) * (((current_time - last_jump_time) - (jump_duration / 2))**2) + 1))
-    player_pos.y = 16 - round(jump_height) - base_height - 2
-    if not check_fit(player_shape, player_pos):
-        is_game_over = True
-    gl.set_shape(player_shape, player_pos, player_color)
-
-def check_fit(shape_matrix: np.ndarray, position: gl.Vec):
-    for x in range(shape_matrix.shape[0]):
-        for y in range(shape_matrix.shape[1]):
-            color = gl.get_pixel(position.x + x, position.y + y)
-            if (
-                shape_matrix[x, y] == 1
-                and (color != gl.colors["background"] and color != player_color)
-            ):
-                return False
-    return True
-
-def world_step():
-    for obst in obstacle_instances:
-        gl.set_shape(obstacles[obst[1]], obst[0], gl.colors["background"])
-    for obst in obstacle_instances:
-        obst[0].x -= 1
-    for obst in obstacle_instances:
-        gl.set_shape(obstacles[obst[1]], obst[0], obstacle_color)
-
-def spawn_step():
-    global spawn_interval
-    obstacle_instances.append((gl.Vec(16, 16 - base_height - 3), rng.randint(0, len(obstacles) - 1)))
-    spawn_interval = obs_spawn_interval_min + rng.random() * (obs_spawn_interval_max - obs_spawn_interval_min)
-
-player_shape = np.array(
-    [
-        [1, 1],
-        [1, 1],
-    ])
-
-obstacles = [
-    np.array([
-        [1, 1],
-        [1, 1],
-        [1, 1]
-    ]).T,
-    np.array([
-        [0, 0],
-        [1, 1],
-        [1, 1]
-    ]).T,
-    np.array([
-        [0, 1],
-        [1, 1],
-        [1, 1]
-    ]).T,
-    np.array([
-        [0, 1],
-        [1, 0],
-        [0, 1]
-    ]).T,
-]
-
-obstacle_instances : list[tuple[gl.Vec, int]] = []
-
-def jmp_formula(x):
-    a = jump_duration
-    return  
+    def player_clock_step(self, current_time):
+        if current_time - self.last_player_step_time >= self.player_step_interval:
+            self.last_player_step_time = current_time
+            self.player_step(current_time)
 
 
-def start_dino():
-    global running, is_game_over
-    running = True
-    restart()
-    il.initialise()
-    for y in range(base_height + 1):
-        for x in range(16):
-            gl.set_pixel(x, 16 - y, ground_color)
-    while running:
-        if il.inputs["exit"]:
-            running = False
-            return False
-        if il.inputs["escape"]:
-            running = False
-        if is_game_over:
-            if il.inputs["space"]:
-                restart()
-                is_game_over = False
-        else:
-            current_time = time.time()
-            if il.inputs["up"] or jump_directly:
-                on_jump(current_time)
-            world_clock_step(current_time)
-            spawn_clock_step(current_time)
-            player_clock_step(current_time)
-        il.reset_inputs()
-        display.show()
-        time.sleep(0.01)
-    remote.unbind_all(il.register_input)
+    def on_jump(self, current_time):
+        delta_time = current_time - self.last_jump_time
+        if delta_time >= self.jump_duration:
+            self.last_jump_time = current_time
+            self.jump_directly = False
+        elif self.jump_duration - delta_time <= 0.1:
+            self.jump_directly = True
+
+
+    def world_clock_step(self, current_time):
+        if current_time - self.last_world_step_time >= self.world_step_interval:
+            self.last_world_step_time = current_time
+            self.world_step()
+
+
+    def spawn_clock_step(self, current_time):
+        if current_time - self.last_spawn_time >= self.spawn_interval:
+            self.last_spawn_time = current_time
+            self.spawn_step()#
+
+
+    def player_step(self, current_time):
+        if not self.check_fit(self.player_shape, self.player_pos):
+            self.is_game_over = True
+        gl.set_shape(self.player_shape, self.player_pos, gl.colors["background"])
+        jump_height = max(0, self.max_jmp_height * (-(4 / (self.jump_duration**2)) * (((current_time - self.last_jump_time) - (self.jump_duration / 2))**2) + 1))
+        self.player_pos.y = 16 - round(jump_height) - self.base_height - 2
+        if not self.check_fit(self.player_shape, self.player_pos):
+            self.is_game_over = True
+        gl.set_shape(self.player_shape, self.player_pos, self.player_color)
+        if self.is_game_over:
+            self.world_draw()
+
+
+    def check_fit(self, shape_matrix: np.ndarray, position: gl.Vec):
+        for x in range(shape_matrix.shape[0]):
+            for y in range(shape_matrix.shape[1]):
+                color = gl.get_pixel(position.x + x, position.y + y)
+                if (
+                    shape_matrix[x, y] == 1
+                    and (color != gl.colors["background"] and color != self.player_color)
+                ):
+                    return False
+        return True
+    
+
+    def world_step(self):
+        for obst in self.obstacle_instances:
+            gl.set_shape(self.obstacles[obst[1]], obst[0], gl.colors["background"])
+        for obst in self.obstacle_instances:
+            obst[0].x -= 1
+        self.world_draw()
+
+    def world_draw(self):
+        for obst in self.obstacle_instances:
+            gl.set_shape(self.obstacles[obst[1]], obst[0], self.obstacle_color)
+
+
+    def spawn_step(self):
+        self.obstacle_instances.append((gl.Vec(16, 16 - self.base_height - 5), rng.randint(0, len(self.obstacles) - 1)))
+        self.spawn_interval = self.obs_spawn_interval_min + rng.random() * (self.obs_spawn_interval_max - self.obs_spawn_interval_min)
+
+
+    def initialise(self):
+        for obst in self.obstacle_instances:
+            gl.set_shape(self.obstacles[obst[1]], obst[0], gl.colors["background"])
+        self.obstacle_instances.clear()
+        super().initialise()
+        for y in range(self.base_height + 1):
+            for x in range(16):
+                gl.set_pixel(x, 16 - y, self.ground_color)
+
+
+    def update(self):
+        current_time = time.time()
+        if il.inputs["up"] or self.jump_directly:
+            self.on_jump(current_time)
+        self.player_clock_step(current_time)
+        self.world_clock_step(current_time)
+        self.spawn_clock_step(current_time)
+
+    def render(self):
+        gl.show()
 
 
 # program
@@ -179,7 +185,9 @@ if __name__ == "__main__":
     remote.start_pygame_thread()
 
     print("startup")
-    start_dino()
+    dino = Dino()
+    dino.initialise()
+    dino.play()
     print("exited")
 
     remote.close_pygame_thread()
